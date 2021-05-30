c511ada498464c6cbd2b2d4bd87b5d594442098a
isminiz <email_adresiniz>
2021-05-30 18:25:02 +0300

    ilk surum

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("git den merhaba!");
         System.out.println("github dan merhaba!");
+
     }
 }
